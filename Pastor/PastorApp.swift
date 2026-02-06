baa562819dbffaab318ae8bc92b44e7bde53e5aa
Victor <vd@tchop.io>
2026-02-06 15:53:22 +0200

    a

--- Pastor/PastorApp.swift
+++ Pastor/PastorApp.swift
@@ -11,8 +11,16 @@ import SwiftUI
 struct MenuBarApp: App {
     @StateObject private var watcher = ClipboardWatcher()
     
+    #if DEBUG
+    private let menuBarIcon = "MenuBarIcon.debug"
+    private let menuBarTitle = "My Utility (DEBUG)"
+    #else
+    private let menuBarIcon = "MenuBarIcon"
+    private let menuBarTitle = "My Utility"
+    #endif
+    
     var body: some Scene {
-        MenuBarExtra("My Utility", image: "MenuBarIcon") {
+        MenuBarExtra(menuBarTitle, image: menuBarIcon) {
             ContentView()
                 .environmentObject(watcher)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
